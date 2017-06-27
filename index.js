efb386040e1ec8473294bf6a6b5be639b0080f6e
Myles O'Connor <myles@gumgum.com>
2017-06-27 12:07:19 -0700

    Removed rollup

--- index.js
+++ index.js
@@ -1,27 +1,63 @@
+const fs = require('fs');
 const imagemin = require('imagemin');
 const imageminJpegtran = require('imagemin-jpegtran');
 const imageminPngquant = require('imagemin-pngquant');
+const aws = require('aws-sdk');
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
-const TEMP_SAMPLE_IMAGE = '/Users/myles/Documents/Repos/image-crusher-node/images/originals/sample.png';
+exports.crushImages = function(event, context, callback) {
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    const params = {
+        Bucket: bucket,
+        Key: key,
+    };
 
-(function() {
-    const imagePath = TEMP_SAMPLE_IMAGE; // Path to the image
-    const imageExtension = imagePath.substr(imagePath.length - 3);
+    s3.getObject(params, (err, data) => {
+        if (err) {
+            console.log(err);
+            const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
+            console.log(message);
+            callback(message);
+        } else {
+            const imagePath = params.Key; // Path to the image
+            console.log('data:', data); // Remove this
 
-    function crush(path) {
-        imagemin([ path ], 'images/crushed', { // Where to save crushed file
-            plugins: [
-                imageminJpegtran(),
-                imageminPngquant({ quality: '65-80' })
-            ]
-        }).then(file => {
-            console.log(file);
-        }).catch(err => {
-            console.log('Error:', err);
-        });
-    }
+            function crush(path) {
+                console.log(`Attempting to crush file ${path}.`);
+                imagemin([ path ], {
+                    plugins: [
+                        imageminJpegtran(),
+                        imageminPngquant({ quality: '65-80' })
+                    ]
+                }).then(file => {
+                    console.log(`Attempting to save crushed file to ${params.Key}.`);
 
-    (imageExtension === 'jpg' || imageExtension === 'png')
-        ? crush(imagePath)
-        : console.log("File is not of type '.jpg' or '.png'.");
-}());
+                    const crushedFile = {
+                        Body: file.data,
+                        Bucket: params.Bucket,
+                        Key: params.Key
+                    };
+
+                    // Save crushed file to S3
+                    s3.putObject(crushedFile, function(err, data) {
+                        if (err) console.log(err, err.stack); // an error occurred
+                        else console.log(data); // successful response
+                    });
+
+                    console.log(file);
+                }).catch(err => {
+                    console.log(`There was an error crushing file ${path}.`);
+                    console.log('Error:', err);
+                });
+            }
+
+            if (data.ContentType === 'image/jpg' || data.ContentType === 'image/png') {
+                console.log('File is a jpg or png, attempting to crush.');
+                crush(imagePath)
+            }
+
+            callback(null, data.ContentType);
+        }
+    });
+}
