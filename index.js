1c14542e28cc3f03da10f1196ce99b53be373d67
Myles O'Connor <myles@gumgum.com>
2017-06-28 10:56:43 -0700

    Updated to use buffer instead of file path, to work in serverless environment

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const imagemin = require('imagemin');
 const imageminJpegtran = require('imagemin-jpegtran');
 const imageminPngquant = require('imagemin-pngquant');
@@ -10,51 +9,43 @@ exports.crushImages = function(event, context, callback) {
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
     const params = {
         Bucket: bucket,
-        Key: key,
+        Key: key
     };
 
     s3.getObject(params, (err, data) => {
         if (err) {
-            console.log(err);
-            const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
-            console.log(message);
+            console.log(`Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`, err);
             callback(message);
         } else {
-            const imagePath = params.Key; // Path to the image
-            console.log('data:', data); // Remove this
-
-            function crush(path) {
-                console.log(`Attempting to crush file ${path}.`);
-                imagemin([ path ], {
+            function crush(buffer) {
+                console.log('Attempting to crush file.');
+                imagemin.buffer(buffer, {
                     plugins: [
                         imageminJpegtran(),
                         imageminPngquant({ quality: '65-80' })
                     ]
                 }).then(file => {
-                    console.log(`Attempting to save crushed file to ${params.Key}.`);
-
                     const crushedFile = {
-                        Body: file.data,
+                        Body: file,
                         Bucket: params.Bucket,
                         Key: params.Key
                     };
 
-                    // Save crushed file to S3
-                    s3.putObject(crushedFile, function(err, data) {
-                        if (err) console.log(err, err.stack); // an error occurred
-                        else console.log(data); // successful response
-                    });
+                    console.log('Attempting to save crushed file.', 'crushedFile:', crushedFile);
 
-                    console.log(file);
+                    s3.putObject(crushedFile, function(err, data) {
+                        (err)
+                        ? console.log('There was an error during s3.putObject', err, err.stack)
+                        : console.log('s3.putObject successful', 'data:', data);
+                    });
                 }).catch(err => {
-                    console.log(`There was an error crushing file ${path}.`);
-                    console.log('Error:', err);
+                    console.log('There was an error while crushing the file.', 'Error:', err);
                 });
             }
 
             if (data.ContentType === 'image/jpg' || data.ContentType === 'image/png') {
-                console.log('File is a jpg or png, attempting to crush.');
-                crush(imagePath)
+                console.log('File is a jpg or png, attempting to crush.', 'data:', data);
+                crush(data.Body); // buffer of file
             }
 
             callback(null, data.ContentType);
